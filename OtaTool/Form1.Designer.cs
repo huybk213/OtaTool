91002af90d77332b81bfa95e6d8ebc2c3a40c1c4
huytv <huytv@vienthongplus.vn>
2022-02-11 21:44:18 +0700

    Add project files.

---
+++ OtaTool/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace OtaTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBrowseFile = new DevExpress.XtraEditors.SimpleButton();
+            this.textBoxPath = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonBrowseFile
+            // 
+            this.buttonBrowseFile.Location = new System.Drawing.Point(508, 25);
+            this.buttonBrowseFile.Name = "buttonBrowseFile";
+            this.buttonBrowseFile.Size = new System.Drawing.Size(75, 45);
+            this.buttonBrowseFile.TabIndex = 5;
+            this.buttonBrowseFile.Text = "Browse file";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(115, 25);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(325, 45);
+            this.textBoxPath.TabIndex = 8;
+            this.textBoxPath.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 278);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.buttonBrowseFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton buttonBrowseFile;
+        private System.Windows.Forms.RichTextBox textBoxPath;
+    }
+}
+
